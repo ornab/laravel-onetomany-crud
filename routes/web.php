bc08b318e5cc55f90a30c78a30f837cf5ceb505d
ornab <hzamil17@gmail.com>
2017-05-14 02:23:20 +0600

    modified

--- routes/web.php
+++ routes/web.php
@@ -23,10 +23,54 @@ Route::get('/create', function(){
     
     $user = User::findOrFail(1);
     
-    $post = new Post(['title' => 'PHP with Laravel', 'body'=>'Laravel is the best framework of PHP']);
+    $post = new Post(['title' => 'PHP with Laravel 2', 'body'=>'Laravel is the best thing to be happen with PHP']);
     
     
     $user->posts()->save($post);
     
     
 });
+
+Route::get('/read', function(){
+    
+    $user = User::findOrFail(1);
+    
+    //return $user->posts;
+    
+    foreach($user->posts as $post){
+        
+        echo $post->title . '<br>';
+        
+    }
+    
+    
+});
+
+Route::get('/update', function(){
+    
+    $user = User::find(1);
+    
+    $user->posts()->whereId(1)->update(['title'=>'I love Laravel', 'body'=>'This is awesome, thanks Taylor Otwell']);
+    
+    
+    
+});
+
+
+Route::get('/delete', function(){
+    
+    $user = User::findOrFail(1);
+    
+    $user->posts()->whereId(1)->delete();
+    
+    return "Done!";
+    
+});
+
+
+
+
+
+
+
+
